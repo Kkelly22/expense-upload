0e3cbf028371c2c611c1ade5f43c046f5979d00e
Kkelly22 <kk504408@gmail.com>
2019-07-27 18:50:40 -0400

    added json render after creation

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,13 @@
 class ExpensesController < ApplicationController
+	protect_from_forgery with: :null_session
+	
 	def create
 		expense = Expense.create(expense_params)
+		if expense && expense.valid?
+			render json: expense, status: 201
+		else
+			render json: {error: "Failed to Upload"}, status: 400
+		end
 	end
 
 
