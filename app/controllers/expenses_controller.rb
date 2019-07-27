7fd70ddcbed1a884a9c27fa7b6d93e0f3959de9b
Kkelly22 <kk504408@gmail.com>
2019-07-27 13:29:57 -0400

    stubbed out a basic expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,12 @@
 class ExpensesController < ApplicationController
+	def create
+		expense = Expense.create(expense_params)
+	end
+
+
+	private
+
+	def expense_params
+		params.require(:expense).permit(:name, :email, :phone_number, :description, :attachment)
+	end
 end
