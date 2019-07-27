7b2414860320e2b53c11b30b8d61d4cf5273c8b7
Kkelly22 <kk504408@gmail.com>
2019-07-27 13:34:17 -0400

    stubbed out a basic expense container

---
+++ client/containers/ExpensesContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+
+class ExpensesContainer extends Component {
+
+  render() {
+    return (
+      <div>
+      	<ExpenseInput/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps =  state  => ({ 
+  expenses: state.expenses
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchExpenses
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpensesContainer)
