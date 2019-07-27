0444d8088f0d600ddb26d5c8b324afc6b258f531
Kkelly22 <kk504408@gmail.com>
2019-07-27 13:48:31 -0400

    stubbed out basic expense actions

---
+++ client/actions/expenseActions.js
@@ -0,0 +1,62 @@
+
+export const fetchExpenses = () => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return dispatch => {
+    fetch(`expenses`, data)
+      .then(response => response.json())
+      .then(expenses => dispatch({
+          type: 'FETCH_EXPENSES',
+          payload: expenses
+      }))
+      .catch(err => err)
+  }
+}
+
+export const createExpense = expense => {
+  let data = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ expense })
+  }
+  
+  return dispatch => {
+    fetch(`expenses`, data)
+      .then(response => response.json())
+      .then(expense => {
+        dispatch({
+              type: 'CREATE_EXPENSE',
+              payload: expense
+            })})
+      .catch(err => err)
+  }
+}
+
+export const deleteExpense = expense => {
+  let data = {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return dispatch => {
+    fetch(`expenses/${expense.id}`, data)
+      .then(response => response.json())
+      .then(expense => dispatch({
+        type: 'DELETE_EXPENSE',
+        payload: expense.id
+      }))
+      .catch(err => err)
+  }
+}
