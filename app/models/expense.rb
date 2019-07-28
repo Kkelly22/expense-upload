fc5195e7c3c5aaf07f8a9db1e10cd8d632ce9ec4
Kkelly22 <kk504408@gmail.com>
2019-07-28 10:03:47 -0400

    added alphanumeric regex validation to phone number

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,11 @@ class Expense < ApplicationRecord
 	validates :attachment, presence: true
 	validates :phone_number, telephone_number: {country: :us, types: [:area_code_optional, :fixed_line, :mobile, :personal_number]}
 	validates :email, presence: true, format: { with: /\b(@cmecorp)\b/, message: "must be of format @cmecorp" }
+	validate :phone_number_without_alpha
+
+	def phone_number_without_alpha
+		if !phone_number.scan(/[a-zA-Z]/).empty?
+			errors.add(:phone_number, "numerics found")
+		end
+	end
 end
