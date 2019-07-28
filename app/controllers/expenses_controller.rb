2e05c1790d02ef9356ebf3f9dd194fcb1babc06f
Kkelly22 <kk504408@gmail.com>
2019-07-28 14:12:12 -0400

    added temporary directory to save file uploads to

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,10 +3,21 @@ class ExpensesController < ApplicationController
 	
 	def create
 		expense = Expense.create(expense_params)
+
 		if expense && expense.valid?
+			upload(expense.attachment)
+			ExpenseMailer.notify(expense).deliver
 			render json: expense, status: 201
 		else
 			render json: {error: 'Failed to Upload'}, status: 400
+		end
+	end
+
+	def upload(file)
+		dir = Rails.root.join('public', 'uploads')
+		Dir.mkdir(dir) unless Dir.exist?(dir)
+		File.open(dir.join(file), 'r+b') do |file|
+			file.write(file.read)
 		end
 	end
 
