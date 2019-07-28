9a94c952b3f8e6ff1ab2418b827316f3c60201f3
Kkelly22 <kk504408@gmail.com>
2019-07-28 14:15:18 -0400

    added alphanumeric validation in phone number, temporarily commented out email validation for testing

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
 	validates :attachment, presence: true
 	validates :phone_number, telephone_number: {country: :us, types: [:area_code_optional, :fixed_line, :mobile, :personal_number]}
-	validates :email, presence: true, format: { with: /\b(@cmecorp)\b/, message: "must be of format @cmecorp" }
+#	validates :email, presence: true, format: { with: /\b(@cmecorp)\b/, message: "must be of format @cmecorp" }
 	validate :phone_number_without_alpha
 
 	def phone_number_without_alpha
