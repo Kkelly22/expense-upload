cb3fd70f67029865b02a77aa088408e25c5b86f2
Kkelly22 <kk504408@gmail.com>
2019-07-28 19:10:22 -0400

    finalized pixel coordinates

--- app/mailers/expense_mailer.rb
+++ app/mailers/expense_mailer.rb
@@ -17,12 +17,12 @@ class ExpenseMailer < ApplicationMailer
   	end
 
   	def create_pdf(background, path, description, price, date)
-		Prawn::Document.generate(path, :page_layout => :portrait) do
-			image background, :image_position => [1,1], :fit => [500,650]
-			draw_text description, :at => [260,450]
-			draw_text price, :at => [250,450]
-			draw_text price, :at => [240,450]
-			draw_text date, :at => [270,450]
-		end
+  		Prawn::Document.generate(path, :page_layout => :portrait) do
+  			image background, :image_position => [1,1], :fit => [500,650]
+  			draw_text description, :at => [100,520]
+  			draw_text price, :at => [400,520]
+  			draw_text price, :at => [400,220]
+  			draw_text date, :at => [30,520]
+  		end
   	end
 end
