8f962a447c48956be9c10aaa9930bc7be4c882dc
Kkelly22 <kk504408@gmail.com>
2019-07-28 21:32:35 -0400

    customized the email message

--- app/mailers/expense_mailer.rb
+++ app/mailers/expense_mailer.rb
@@ -2,26 +2,33 @@ class ExpenseMailer < ApplicationMailer
 	default from: 'kk504408@gmail.com'
 	
 	def notify(expense)
+      @expense = expense
+
+      #variables
     	attachment = expense.attachment
     	description = expense.description[0..25]
-    	price = "$40.00"
+    	cost = expense.cost
     	date = Time.now.strftime("%m/%d/%Y")
 
+      #locations
     	background = Rails.root.join('public', 'expense-report-img.png')
     	path = Rails.root.join('public', 'expense-report.pdf')
-    	create_pdf(background, path, description, price, date)
 
+      #create pdf
+    	create_pdf(background, path, description, cost, date)
+
+      #send mailer
     	attachments.inline[attachment] = File.read(path)
-   		mail(to: expense.email,subject: "Expense submitted by" + expense.name + "for" + expense.description)
+   		mail(to: expense.email,subject: "Expense submitted by " + expense.name + " for " + expense.description)
   	end
 
-  	def create_pdf(background, path, description, price, date)
+  	def create_pdf(background, path, description, cost, date)
   		Prawn::Document.generate(path, :page_layout => :portrait) do
   			image background, :image_position => [1,1], :fit => [500,650]
   			draw_text description, :at => [100,520]
-  			draw_text price, :at => [400,520]
-  			draw_text price, :at => [340,520]
-        draw_text price, :at => [400,220]
+  			draw_text cost, :at => [400,520]
+  			draw_text cost, :at => [340,520]
+        draw_text cost, :at => [400,220]
   			draw_text date, :at => [30,520]
   		end
   	end
