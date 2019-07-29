9c6de72ac9e47f7478e369b0fd0855fd295b667e
Kkelly22 <kk504408@gmail.com>
2019-07-28 20:01:10 -0400

    bug fixes to pdf generator and mailer

--- app/mailers/expense_mailer.rb
+++ app/mailers/expense_mailer.rb
@@ -2,18 +2,17 @@ class ExpenseMailer < ApplicationMailer
 	default from: 'kk504408@gmail.com'
 	
 	def notify(expense)
-    	@expense = expense
-    	@attachment = expense.attachment
-    	description = "test description upload"
+    	attachment = expense.attachment
+    	description = expense.description[0..25]
     	price = "$40.00"
-    	date = "07/28/2019"
+    	date = Time.now.strftime("%m/%d/%Y")
 
     	background = Rails.root.join('public', 'expense-report-img.png')
     	path = Rails.root.join('public', 'expense-report.pdf')
     	create_pdf(background, path, description, price, date)
 
-    	attachments.inline[@attachment] = File.read(path)
-   		mail(to: @expense.email,subject: "Notification")
+    	attachments.inline[attachment] = File.read(path)
+   		mail(to: expense.email,subject: "Notification")
   	end
 
   	def create_pdf(background, path, description, price, date)
@@ -21,7 +20,8 @@ class ExpenseMailer < ApplicationMailer
   			image background, :image_position => [1,1], :fit => [500,650]
   			draw_text description, :at => [100,520]
   			draw_text price, :at => [400,520]
-  			draw_text price, :at => [400,220]
+  			draw_text price, :at => [340,520]
+        draw_text price, :at => [400,220]
   			draw_text date, :at => [30,520]
   		end
   	end
